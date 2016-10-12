ab2a941d6051e1e10752a79c94e0d1746f7b470f
Qun Sun <qsun@esri.com>
2016-10-12 10:33:57 +0800

    make clear task work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 ﻿var gulp = require("gulp");
+var path = require("path");
+var fs = require("fs");
+var rmdir = require("rmdir");
 var exec = require('child_process').exec;
 
 gulp.task("default", ["build"]);
 
-gulp.task("clear", function(){
-  console.log("execute clear task");
+gulp.task("clear", function(cb){
+  var p = path.join(__dirname, "buildOutput");
+  if(fs.existsSync(p)){
+    rmdir(p, function(err, dirs, files){
+      if(dirs){
+        console.log(dirs);
+      }
+      if(files){
+        console.log(files);
+      }
+      cb(err);
+    });
+  }else{
+    cb();
+  }
 });
 
 gulp.task("build", ["clear"], function(cb){
@@ -12,8 +28,12 @@ gulp.task("build", ["clear"], function(cb){
   //return gulp.src("js/**/*.js").pipe(concat("world.js")).pipe(gulp.dest("build"));
   //r.js.cmd -o build-config.js
   exec("node node_modules/requirejs/bin/r.js -o build-config.js", function(err, stdout, stderr){
-    console.log(stdout);
-    console.log(stderr);
+    if(stdout){
+      console.log(stdout);
+    }
+    if(stderr){
+      console.log(stderr);
+    }
     cb(err);
   });
 });
