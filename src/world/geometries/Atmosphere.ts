b10c0357643448ddde5e1bf41d8abf2452a1c813
Qun Sun <qsun@esri.com>
2016-12-01 13:12:31 +0800

    add Atmosphere files,#16

---
+++ src/world/geometries/Atmosphere.ts
@@ -0,0 +1,13 @@
+﻿///<amd-module name="world/geometries/Atmosphere"/>
+
+import Vertice = require("./MeshVertice");
+import Triangle = require("./Triangle");
+import Mesh = require("./Mesh");
+
+class Atmosphere  extends Mesh {
+  constructor(public vertices: Vertice[], public triangles: Triangle[]) {
+    super();
+  }
+}
+
+export = Atmosphere;
