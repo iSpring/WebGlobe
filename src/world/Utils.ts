27a7d112ec9ef6dce30399e088194d73af4cd3e4
Qun Sun <qsun@esri.com>
2016-12-30 13:45:37 +0800

    convert Utils from Object to static class

--- src/world/Utils.ts
+++ src/world/Utils.ts
@@ -4,23 +4,19 @@ type ArrayVoidCallbackFunction = (value: any, index: number, arr: Array<any>) =>
 type ArrayBooleanCallbackFunction = (value: any, index: number, arr: any[]) => boolean;
 type ArrayAnyCallbackFunction = (value: any, index: number, arr: any[]) => any;
 
-const Utils = {
-    GREATER: "GREATER",
-    GEQUAL: "GEQUAL",
-    LESS: "LESS",
-    LEQUAL: "LEQUAL",
+class Utils {
 
-    isNumber(v: any): boolean {
-        return typeof v == "number";
-    },
+    static isNumber(v: any): boolean {
+        return typeof v === "number";
+    }
 
-    isInteger(v: any): boolean {
+    static isInteger(v: any): boolean {
         var isInt = false;
         var isNum = this.isNumber(v);
         if (isNum) {
             var numFloat = parseFloat(v);
             var numInt = parseInt(v);
-            if (numFloat == numInt) {
+            if (numFloat === numInt) {
                 isInt = true;
             }
             else {
@@ -31,82 +27,62 @@ const Utils = {
             isInt = false;
         }
         return isInt;
-    },
+    }
 
-    judgeNumberBoundary(num: number, operator: any, boundry: number): boolean {
-        if (operator != this.GREATER && operator != this.GEQUAL && operator != this.LESS && operator != this.LEQUAL) {
-            throw "operator is invalid";
-        }
-        var b: boolean;
-        if (operator == this.GREATER) {
-            b = num > boundry;
-        }
-        else if (operator == this.GEQUAL) {
-            b = num >= boundry;
-        }
-        else if (operator == this.LESS) {
-            b = num < boundry;
-        }
-        else if (operator == this.LEQUAL) {
-            b = num <= boundry;
-        }
-        return b;
-    },
+    static isPositive(v: number): boolean {
+        return v > 0;
+    }
 
-    isPositive(v: number): boolean {
-        return this.judgeNumberBoundary(v, this.GREATER, 0);
-    },
+    static isNegative(v: number): boolean {
+        return v < 0;
+    }
 
-    isNegative(v: number): boolean {
-        return this.judgeNumberBoundary(v, this.LESS, 0);
-    },
+    static isNonNegative(v: number): boolean {
+        return v >= 0;
+    }
 
-    isNonNegative(v: number): boolean {
-        return this.judgeNumberBoundary(v, this.GEQUAL, 0);
-    },
+    static isNonPositive(v: number): boolean {
+        return v <= 0;
+    }
 
-    isNonPositive(v: number): boolean {
-        return this.judgeNumberBoundary(v, this.LEQUAL, 0);
-    },
-
-    isPositiveInteger(v: number): boolean {
+    static isPositiveInteger(v: number): boolean {
         return this.isPositive(v) && this.isInteger(v);
-    },
+    }
 
-    isNonNegativeInteger(v: number): boolean {
+    static isNonNegativeInteger(v: number): boolean {
         return this.isNonNegative(v) && this.isInteger(v);
-    },
+    }
 
-    isArray(v: any): boolean {
+    static isArray(v: any): boolean {
         return Object.prototype.toString.call(v) === '[object Array]';
-    },
+    }
 
-    isFunction(v: any) {
+    static isFunction(v: any) {
         return typeof v === 'function';
-    },
+    }
 
-    forEach(arr: ArrayLike<any>, func: ArrayVoidCallbackFunction): void {
+    static forEach(arr: ArrayLike<any>, func: ArrayVoidCallbackFunction): void {
         return this.isFunction((<any>arr).forEach) ? (<any>arr).forEach(func) : Array.prototype.forEach.call(arr, func);
-    },
+    }
 
-    filter(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): any[] {
+    static filter(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): any[] {
         return this.isFunction((<any>arr).filter) ? (<any>arr).filter(func) : Array.prototype.filter.call(arr, func);
-    },
+    }
 
-    map(arr: ArrayLike<any>, func: ArrayAnyCallbackFunction): any[] {
+    static map(arr: ArrayLike<any>, func: ArrayAnyCallbackFunction): any[] {
         return this.isFunction((<any>arr).map) ? (<any>arr).map(func) : Array.prototype.map.call(arr, func);
-    },
+    }
 
-    some(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): boolean {
+    static some(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): boolean {
         return this.isFunction((<any>arr).some) ? (<any>arr).some(func) : Array.prototype.some.call(arr, func);
-    },
+    }
 
-    every(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): boolean {
+    static every(arr: ArrayLike<any>, func: ArrayBooleanCallbackFunction): boolean {
         return this.isFunction((<any>arr).every) ? (<any>arr).every(func) : Array.prototype.every.call(arr, func);
-    },
+    }
 
     //过滤掉数组中重复的元素
-    filterRepeatArray(arr: Array<any>): any[] {
+    static filterRepeatArray(arr: Array<any>): any[] {
         var cloneArray = arr.map(function (item: any) {
             return item;
         });
@@ -122,9 +98,9 @@ const Utils = {
             cloneArray.splice(0, 1);
         }
         return simplifyArray;
-    },
+    }
 
-    jsonp(url: string, callback: (response: any) => void, callbackParameterName: string = "cb"): () => void {
+    static jsonp(url: string, callback: (response: any) => void, callbackParameterName: string = "cb"): () => void {
         var callbackName = `webglobe_callback_` + Math.random().toString().substring(2);
         if (url.indexOf('?') < 0) {
             url += '?';
@@ -150,9 +126,9 @@ const Utils = {
         return function () {
             canceled = true;
         };
-    },
+    }
 
-    isMobile(): boolean {
+    static isMobile(): boolean {
         return !!window.navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone|IEMobile|Opera Mini/i);
     }
 };
