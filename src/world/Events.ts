d8cca4d4ce015c9430c779315b22a8bfefb6c6da
Qun Sun <qsun@esri.com>
2017-02-14 12:43:10 +0800

    return a Handle object for addEventListener method of Events

--- src/world/Events.ts
+++ src/world/Events.ts
@@ -3,6 +3,10 @@ import Utils = require("./Utils");
 
 type Listener = (data: any) => void;
 
+export interface Handle{
+  remove(): void;
+}
+
 export class EventEmitter{
   events:{[key:string]:Listener[]} = null;
   onceListeners:Listener[] = null;
@@ -25,22 +29,25 @@ export class EventEmitter{
     }
   }
 
-  once(eventName:string, listener: Listener){
-    this.addEventListener(eventName, listener);
+  once(eventName:string, listener: Listener): Handle{
     this.onceListeners.push(listener);
+    return this.addEventListener(eventName, listener);
   }
 
-  on(eventName:string, listener: Listener){
-    this.addEventListener(eventName, listener);
+  on(eventName:string, listener: Listener): Handle{
+    return this.addEventListener(eventName, listener);
   }
 
-  addEventListener(eventName:string, listener: Listener){
+  addEventListener(eventName:string, listener: Listener): Handle{
     var listeners = this.events[eventName];
     if(listeners){
       listeners.push(listener);
     }else{
       this.events[eventName] = [listener];
     }
+    var handle: any = {};
+    handle.remove = () => this.removeEventListener(eventName, listener);
+    return handle;
   }
 
   removeEventListener(eventName:string, listener: Listener){
